e780dc44cf3ec8750241f882fbf51dcb67e348eb
OkeOgbe <46296469+OkeOgbe@users.noreply.github.com>
2020-03-13 15:23:25 +0100

    sign up page only

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(".show").on("click", function () {
+    if($(".password").attr("type") == "password"){
+        $(".password").attr("type", "text");
+    }
+    else{
+        $(".password").attr("type", "password");
+    }
+});
