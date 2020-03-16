6f3e6740af7adb6e317ece4bcfbd400783da2edf
OkeOgbe <46296469+OkeOgbe@users.noreply.github.com>
2020-03-16 19:15:34 +0100

    home page redesign initial commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+        $('nav').fadeIn('500', function(){
+            $('nav').addClass('fixed_nav');
+        });  
+    } else {
+      $('nav').removeClass('fixed_nav');
+    }
+  });
+
 $(".show").on("click", function () {
     if($(".password").attr("type") == "password"){
         $(".password").attr("type", "text");
@@ -5,4 +15,16 @@ $(".show").on("click", function () {
     else{
         $(".password").attr("type", "password");
     }
+});
+
+  
+jQuery(".owl-carousel").owlCarousel({
+    dots:true,
+    center: true,
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    items: 3,
 });
